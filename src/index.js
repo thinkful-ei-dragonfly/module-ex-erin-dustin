67042229e7a86825aa3899d773bc8107ddc4bda1
Dustin Haefele <Dustin.haefele@gmail.com>
2019-05-13 12:45:37 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+/* global shoppingList, store, api */
+import $ from 'jquery';
+
+$(document).ready(function() {
+  console.log('working');
+  // shoppingList.bindEventListeners();
+
+  // // On initial load, fetch Shopping Items and render
+  // api.getItems()
+  //   .then((items) => {
+  //     items.forEach((item) => store.addItem(item));
+  //     shoppingList.render();
+  //   })
+  //   .catch(err => console.log(err.message))
+});
+
